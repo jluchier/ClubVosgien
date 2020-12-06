a883cdc01c78dd12e7bf7588a9dddd2b828f9ba9
Balou <bluchier@laposte.net>
2020-12-06 17:17:50 +0100

    Use webp parralax + fix image size

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -3,15 +3,8 @@
 @section('gallery', 'navActive')
 
 @section('content')
-<div class="CV-TopContainerHome">
-    <img class="CV-TopContainerHome-ImgTop" src="/images/common/large1_009_1.jpg" alt="Les vosges">
-    {{-- <picture>
-        <source media="(min-width:650px)" srcset="/images/common/medium_000.jpg">
-        <source media="(min-width:465px)" srcset="/images/common/small_000.jpg">
-        <img src="/images/common/large_000.jpg" alt="Les vosges" style="width:auto;">
-    </picture> --}}
-    <!-- <h2 class="w3-xxxlarge w3-center CV-fontColorRed" style="padding-top:5%" >Bienvenue sur le site du club vosgien RVF</h2> -->
-    <img src="/images/common/wave_white.svg " class="CV-TopContainerHome-ImgBottom">
+<div class="CV-TopContainerHome CV-Bg1">
+    <img src="/images/common/wave_white.svg">
 </div>
 
 
