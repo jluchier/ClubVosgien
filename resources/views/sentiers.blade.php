7e56c4853cbbd4e383a4d1529c2f66ae6ee4616f
papajj <jluchier@free.fr>
2021-01-10 20:16:24 +0100

    Fixes + suite infofede

--- resources/views/sentiers.blade.php
+++ resources/views/sentiers.blade.php
@@ -21,7 +21,7 @@
             </p>
         </div>
         <div class="w3-rest w3-center">
-            <img class="zoom" style="max-width: 50%" src="" alt="image à suivre">
+            <img class="zoom" style="max-width: 50%" src="/images/others/balisage-CV" alt="image à suivre">
         </div>
     </div>
 
@@ -31,10 +31,60 @@
               <img class="zoom" style="max-width: 50%" src="" alt="image à venir">
           </div>
         <div class="w3-half CV-Fond-Carte w3-card-4">
+          <h1>Circuit des chalets</h1>
           <p>
+          Départ :Place Albert Montémont
+            Balisage :
+            Distance : 14,7 km
+            Dénivelé : 636 m
+            Temps : 4h15
+          </p>
+
+
+          <p>
+            Partir par la rue Napoléon Forel, au
+rond-point de la Croix, continuer à
+droite par la rue Louis Wittmann.
+Après 100 m s’engager à gauche sur
+un petit sentier entre transformateur et
+habitation, suivre ce sentier qui passe
+entre propriétés et lisière de forêt,
+traverser une pâture (le passage est
+autorisé en fermant les barrières),
+et après être passé à proximité de
+quelques maisons, vous arrivez sur une
+route.
+Partir à droite sur 20 m puis à gauche,
+vous êtes sortis du village, suivre le balisage qui, à travers prairies et forêts
+le Chalet de la Charmotte, le site de la
+Roche Jaugeon, le Chalet des Haneaux,
+le Chalet de la Vrille (l’un des plus
+tranquilles du secteur, car son accès est
+seulement piétonnier).
+Après avoir rejoint la fin d’une route
+goudronnée, se diriger vers le Chalet
+des Fraiteux qui nécessite un détour de
+100 m pour y accéder, revenir sur ses
+pas et continuer la descente en suivant
+toujours le même balisage pour revenir
+au point de départ.
           </p>
         </div>
       </div>
 </div>
+
+<div class="w3-row-padding w3-container w3-margin">
+    <div class="CV-Fond-Carte w3-card-4 w3-half w3-padding">
+<h1>Circuit : Linqueny</h1>
+<p>
+
+</p>
+</div>
+<div class="w3-rest w3-center">
+    <img class="zoom" style="max-width: 50%" src="/images/others/balisage-CV" alt="image à suivre">
+</div>
+</div>
+
+
 <div class="parallax_3"></div>
 @endsection
