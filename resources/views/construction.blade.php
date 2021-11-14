852d6a5ab3230553bcbb95d079f69c6a7990577b
jluchier <jluchier@free.fr>
2021-11-14 22:05:47 +0100

    White text fixed !

--- resources/views/construction.blade.php
+++ resources/views/construction.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 <div class="building w3-display-container">
-  <div class="middle">
-    <h1>*** (°-°) *** C'est pour bientôt *** \°~°/ ***</h1>
+  <div class="middleBuild">
+    <h1 style="color: white">*** (°-°) *** C'est pour bientôt *** \°~°/ ***</h1>
       <p>Cette page est en construction pour le moment</p>
       <div id="vuejs">
         <time-rest-counter></time-rest-counter>
