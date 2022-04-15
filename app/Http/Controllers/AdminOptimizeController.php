a0f50b4426bc127f679d57e0f7044cf386238bd5
Papajj <jjluchier@sfr.fr>
2022-04-15 23:19:32 +0200

    update optimizeController
    add migration

--- app/Http/Controllers/AdminOptimizeController.php
+++ app/Http/Controllers/AdminOptimizeController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Artisan;
+use Illuminate\Support\Facades\Artisan as FacadesArtisan;
 
 class AdminOptimizeController extends Controller
 {
-    public function optimisation(){
-
-            Artisan::call('optimize');
-            return redirect(route("articles.index"))->with("success",'optimisation faite');
+    public function optimisation()
+    {
+        Artisan::call('migrate');
+        Artisan::call('optimize');
+        return redirect(route("articles.index"))->with("success",'optimisation faite');
     }
 }
