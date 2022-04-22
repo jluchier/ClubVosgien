3b4d31644e8718caf6319dae4b8c8f40cd0e4c59
Papajj <jjluchier@sfr.fr>
2022-04-22 22:02:22 +0200

    CR done ... to be fixed
     on admin\CompteRendus\edit.blade.php

--- resources/views/Includes/compteRenduCard.blade.php
+++ resources/views/Includes/compteRenduCard.blade.php
@@ -1,12 +1,12 @@
-<div class="CV-Fond-Carte w3-card-4 w3-third w3-padding">
-    <div class="w3-theme-dark">
+<div class="CV-Fond-Carte w3-card-4 CV-flex-gallery-column w3-margin">
+    <div class="w3-theme-dark w3-padding">
         <h2>{{ $cr->title }}</h2>
     </div>
     <div class="w3-padding">
         {!!$cr->content!!}
     </div>
     @foreach ($cr->attachables()->get() as $attachment)
-    <div class="w3-row-padding w3-card-4">
+    <div class="w3-padding w3-card-4">
         <a href="{{ Storage::url("CompteRendusFile/".$attachment->name) }}">{{ $attachment->name }}</a>
     </div>
     @endforeach
--- resources/views/compterendus.blade.php
+++ resources/views/compterendus.blade.php
@@ -5,22 +5,13 @@
 @section('content')
 <div class="CV-TopContainerHome parallax_1 w3-display-container">
   <h1 class='parallaxTitle w3-display-middle'>Comptes-rendus des réunions</h1>
-    <img src="/images/common/wave_white.svg">
+  <img src="/images/common/wave_white.svg">
 </div>
 
-<div class="w3-row-padding w3-container w3-margin">
-@foreach ($compterendus as $key => $cr)
-@if ($key %3 == 0)
-
+<div class="CV-flex-gallery-row" style="justify-content:center">
+  @foreach ($compterendus as $cr)
   @include('Includes.compteRenduCard')
-
-  @elseif ($key %3 == 1)
-  @include('Includes.compteRenduCard')
-  @else
-  @include('Includes.compteRenduCard')
-  @endif
   @endforeach
-  
-</div>  
+</div>
 
 @endsection
